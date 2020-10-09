5d5bc2510dc379408995de225cfade92eff2d649
Iswandi Saputra <iswandi.saputra11@gmail.com>
2020-10-09 11:21:14 +0700

    add sharedpreferences

--- lib/screens/splashloading_screen.dart
+++ lib/screens/splashloading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myfirstapp_flutter/screens/home_screen.dart';
+import 'package:myfirstapp_flutter/screens/utama_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 class SplashLoadingScreen extends StatelessWidget {
@@ -7,12 +9,25 @@ class SplashLoadingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SplashScreen(
         seconds: 3,
-        navigateAfterSeconds:  MyHomeApp(),
-        title:  Text('Welcome In Flutter Icon+'),
-        image:  Image.asset('gambar/ojol.png'),
+        navigateAfterSeconds: checkSession(context),
+        title: Text('Welcome In Flutter Icon+'),
+        image: Image.asset('gambar/ojol.png'),
         backgroundColor: Colors.white,
-        styleTextUnderTheLoader:  TextStyle(),
+        styleTextUnderTheLoader: TextStyle(),
         photoSize: 100.0,
         loaderColor: Colors.red);
+  }
+
+  checkSession(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool sesi = prefs.getBool("sesi") ?? false;
+    if (sesi == true) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => UtamaScreen()));
+    } else {
+      prefs.setBool("sesi", true);
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MyHomeApp()));
+    }
   }
 }
